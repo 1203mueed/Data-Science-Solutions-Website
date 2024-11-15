545b45c082ade5fa04a23cb4fb3cf8bea4279e2c
Abdul Mueed <abdulmueed1203@gmail.com>
2024-11-16 02:13:04 +0600

    Added Papers page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Models from './pages/Models';
 import ModelDetailsPage from './pages/ModelDetailsPage';
 import CompetitionPage from './pages/CompetitionPage';
 import Navbar from './components/Navbar';
+import PapersPage from './pages/PapersPage';
 import PricingPage from './pages/PricingPage';
 import ContactPage from './pages/ContactPage';
 import DatasetPage from './pages/DatasetPage';
@@ -27,6 +28,7 @@ function App() {
             <Route path="/pricing" element={<PricingPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/datasets" element={<DatasetPage />} />
+            <Route path="/papers" element={<PapersPage />} />
             <Route path="/work-with-team" element={<WorkWithTeamPage />} />
             {/* Add other routes as needed */}
           </Routes>
